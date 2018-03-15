01b562d21e9250a02c1137fba66e45d788b10185
1187851491 <1187851491@qq.com>
2018-03-15 23:21:22 +0800

    完善流程

--- js/jyc.js
+++ js/jyc.js
@@ -1,24 +1,30 @@
 
-
 $(document).ready(function(){
     $("#topbar-cart").hover(function(){
         $("#load").slideToggle("fast");
     });
-    var xx = $.get();
+    var xx = $.getUrlParam('id');
     console.log(xx);
     if(xx!=null){
+        flag=1;
         $("#login").text("admin");
-       $("#register").text("个人中心");
-        $("#login").attr("href","/index.html");
-        $("#register").attr("href","/views/mobilephone.html");
+       $("#register").text("消息通知");
+       $("#dd").text("我的订单");
+        $("#login").attr("href","/mi/views/Personal_central.html");
+        $("#register").attr("href","/mi/views/xiaoxi.html");
     }
 });
-var id=getUrlParam('id');
 function  navallOver() {
     document.getElementById("navbar").style.display="block";
 }
 function navallOut(){
     document.getElementById("navbar").style.display="none";
 }
-
+(function ($) {
+    $.getUrlParam = function (name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return unescape(r[2]); return null;
+    }
+})(jQuery);
 
