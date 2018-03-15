6b488519310bbd09d47f49f0d98bc18426d627cc
1187851491 <1187851491@qq.com>
2018-03-15 21:46:48 +0800

    修改了登录界面

--- js/jyc.js
+++ js/jyc.js
@@ -4,12 +4,21 @@ $(document).ready(function(){
     $("#topbar-cart").hover(function(){
         $("#load").slideToggle("fast");
     });
+    var xx = $.get();
+    console.log(xx);
+    if(xx!=null){
+        $("#login").text("admin");
+       $("#register").text("个人中心");
+        $("#login").attr("href","/index.html");
+        $("#register").attr("href","/views/mobilephone.html");
+    }
 });
-
+var id=getUrlParam('id');
 function  navallOver() {
     document.getElementById("navbar").style.display="block";
 }
 function navallOut(){
     document.getElementById("navbar").style.display="none";
 }
+
 
